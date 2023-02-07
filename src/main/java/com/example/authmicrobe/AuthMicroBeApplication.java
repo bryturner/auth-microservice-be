1b3590608d4110edb5cb270a27463bd8d5dd7824
Bryan Turner <bryan.turner.ch@gmail.com>
2023-02-06 21:03:11 -0700

    first commit

---
+++ src/main/java/com/example/authmicrobe/AuthMicroBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.authmicrobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthMicroBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthMicroBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/authmicrobe/AuthMicroBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authmicrobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthMicroBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
